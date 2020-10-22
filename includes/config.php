1a56060834ca2583cb57df497bcb943bc399bb37
graciagit <gittagraciaevelyndiva@gmail.com>
2020-10-22 19:04:15 +0700

    Update config.php
    Update name -> Gitta Gracia Evelyn Diva

--- includes/config.php
+++ includes/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website',
+        'name' => 'Gitta Gracia Evelyn Diva',
         'site_url' => '',
         'pretty_uri' => false,
         'nav_menu' => [
